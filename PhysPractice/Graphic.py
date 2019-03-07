107d419d0a82264140207b8d51e91123f5da3820
Igor Grebennikov <cidor361@gmail.com>
2019-03-08 00:01:23 +0300

    some fixes

--- PhysPractice/Graphic.py
+++ PhysPractice/Graphic.py
@@ -1,5 +1,6 @@
 import re
 import matplotlib.pyplot as plt
+import numpy as np
 
 reg = re.compile('[^0-9.\s ]')
 
@@ -17,11 +18,19 @@ for i in range(len(array)):
         for k in range(num_of_path):
             array.append([subarray[k * 2], subarray[k * 2 - 1]])
 
+xt = []
+yt = []
 i = 0
 for i in range(len(array)):
     x, y = array[i]
     x = float(x)
     y = float(y)
+    xt.append(x)
+    yt.append(y)
     plt.scatter(x, y)
-
+plt.plot(xt, yt)
+# x = np.arange(0.1, 4, 0.5)
+# y = np.exp(-x)
+# fig, ax = plt.subplots()
+# ax.errorbar(xt, yt, xerr=0.2, yerr=0.4)
 plt.show()
