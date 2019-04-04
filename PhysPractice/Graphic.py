a6200202f93b2950569bc3371909999633ddedbe
Igor Grebennikov <cidor361@gmail.com>
2019-04-04 18:29:16 +0300

    For PhysPractice:     * redo some methods

--- PhysPractice/Graphic.py
+++ PhysPractice/Graphic.py
@@ -4,26 +4,39 @@ import numpy as np
 
 reg = re.compile('[^0-9.\s]')
 
-with open("data.txt") as file:
-    array = [row.strip() for row in file]
-print(array)
-for i in range(len(array)):
-    array[i] = array[i].replace("(", "")
-    array[i] = array[i].replace(")", "")
-    print(array[i])
-
+f = open('data.txt', 'r')
+# data = f.read()
+# data = reg.sub('', data)
 i = 0
-for i in range(len(array)):
-    # array[i] = reg.sub('', array[i]).split(' ')
-    if array[i].count(' ') > 2:
-        num_of_path = array[i].count(' ') // 2
-        subarray = array[i]
-        print(subarray)
-        del array[i]
-        k = 0
-        for k in range(num_of_path):
-            array.append([subarray[k * 2], subarray[k * 2 - 1]])
-print(array)
+data = []
+for line in f:
+    if line.count(' ') < 3:
+        data.append(reg.sub('', line))
+    else:
+        spaces = line.count(' ')
+        cut = spaces // 2
+
+# print(data)
+# with open("data.txt") as file:
+#     array = [row.strip() for row in file]
+# print(array)
+# for i in range(len(array)):
+#     array[i] = array[i].replace("(", "")
+#     array[i] = array[i].replace(")", "")
+#     print(array[i])
+
+# i = 0
+# for i in range(len(array)):
+#     # array[i] = reg.sub('', array[i]).split(' ')
+#     if array[i].count(' ') > 2:
+#         num_of_path = array[i].count(' ') // 2
+#         subarray = array[i]
+#         print(subarray)
+#         del array[i]
+#         k = 0
+#         for k in range(num_of_path):
+#             array.append([subarray[k * 2], subarray[k * 2 - 1]])
+# print(array)
 # coordinates = []
 # i = 0
 # for i in range(len(array)):
