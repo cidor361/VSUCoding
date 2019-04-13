39717f1620d3d7e11757cd9f9af1fe397578588d
Igor Grebennikov <cidor361@gmail.com>
2019-04-13 20:06:35 +0300

    For PhysPractice:     * upgrade reading from file

--- PhysPractice/Graphic.py
+++ PhysPractice/Graphic.py
@@ -2,28 +2,13 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-reg = re.compile('[^0-9.\s]')
+# plt.scatter(x, y)
 
-f = open('data.txt', 'r')
-# data = f.read()
-# data = reg.sub('', data)
-i = 0
-data = []
-for line in f:
-    if line.count(' ') < 3:
-        data.append(reg.sub('', line))
-    else:
-        spaces = line.count(' ')
-        cut = spaces // 2
-
-# print(data)
 # with open("data.txt") as file:
 #     array = [row.strip() for row in file]
-# print(array)
 # for i in range(len(array)):
 #     array[i] = array[i].replace("(", "")
 #     array[i] = array[i].replace(")", "")
-#     print(array[i])
 
 # i = 0
 # for i in range(len(array)):
@@ -31,12 +16,10 @@ for line in f:
 #     if array[i].count(' ') > 2:
 #         num_of_path = array[i].count(' ') // 2
 #         subarray = array[i]
-#         print(subarray)
 #         del array[i]
 #         k = 0
 #         for k in range(num_of_path):
 #             array.append([subarray[k * 2], subarray[k * 2 - 1]])
-# print(array)
 # coordinates = []
 # i = 0
 # for i in range(len(array)):
@@ -45,8 +28,7 @@ for line in f:
 #     y = float(y)
 #     coordinate = [x, y]
 #     coordinates.append(coordinate)
-    # plt.scatter(coordinates[i])
-# print(coordinates)
+# plt.scatter(coordinates[i])
 # plt.plot(xt, yt)
 
 # x = np.arange(0.1, 4, 0.5)
@@ -55,3 +37,89 @@ for line in f:
 # ax.errorbar(xt, yt, xerr=0.2, yerr=0.4)
 
 # plt.show()
+from scipy.interpolate import interp1d
+
+
+def cleanstring(string):
+    string = string.replace("!", "")
+    string = string.replace("@", "")
+    string = string.replace("#", "")
+    string = string.replace("$", "")
+    string = string.replace("%", "")
+    string = string.replace("^", "")
+    string = string.replace("&", "")
+    string = string.replace("*", "")
+    string = string.replace("(", "")
+    string = string.replace(")", "")
+    string = string.replace("+", "")
+    string = string.replace("=", "")
+    string = string.replace("?", "")
+    string = string.replace("\'", "")
+    string = string.replace("\"", "")
+    string = string.replace("{", "")
+    string = string.replace("}", "")
+    string = string.replace("[", "")
+    string = string.replace("]", "")
+    string = string.replace("<", "")
+    string = string.replace(">", "")
+    string = string.replace("~", "")
+    string = string.replace("`", "")
+    string = string.replace(":", "")
+    string = string.replace(";", "")
+    string = string.replace("|", "")
+    string = string.replace("\\", "")
+    string = string.replace("/", "")
+    string = string.replace(",", "")
+    string = string.strip(' ')
+    return string
+
+
+def get_from_string(string, data):
+    string.strip(' ')
+    gaps = string.count(' ') // 2 + 1
+    subdata = string.split(' ')
+    i = 0
+    for i in range(gaps):
+        x = subdata[0]
+        y = subdata[1]
+        x = float(x)
+        y = float(y)
+        del subdata[0]
+        del subdata[0]
+        data.append([x, y])
+    return data
+
+
+def get_from_file(file, data):
+    for string in file:
+        string = cleanstring(string)
+        if string.count(' ') < 2:
+            x, y = string.split(' ')
+            x = float(x)
+            y = float(y)
+            data.append([x, y])
+        else:
+            data = get_from_string(string, data)
+    return data
+
+
+def cut_list(array):
+    i = 0
+    x = []
+    y = []
+    for i in range(len(array)):
+        x.append(array[i][0])
+        y.append(array[i][1])
+    return x, y
+
+
+def getdata():
+    file = open('data.txt', 'r')
+    data = []
+    data = get_from_file(file, data)
+    data.sort()
+    x, y = cut_list(data)
+    f = interp1d(x, y)
+
+
+getdata()
