76a86bef3e69adacf18cef3b9cdc72b5e3004cc2
Igor Grebennikov <cidor361@gmail.com>
2019-04-19 14:48:29 +0300

    For PhysPractice:     * made approximation

--- PhysPractice/Graphic.py
+++ PhysPractice/Graphic.py
@@ -3,12 +3,18 @@ import numpy as np
 from collections import OrderedDict
 
 
+
+
+
 def cleanstring(string):
     string = string.replace("(", "")
     string = string.replace(")", "")
     string = string.replace("\n", " ")
     string = string.strip(' ')
     return string
+
+
+
 
 
 def get_from_file(file, data):
@@ -76,14 +82,39 @@ def lagranz(x, y, t):
     return z
 
 
-x, y = getdata()
-print(x, y)
-plt.scatter(x, y)
 
-# plt.plot(x, y)
+def points():
+    x, y = getdata()
+    plt.scatter(x, y)
+    plt.show()
 
-xnew = np.linspace(np.min(x), np.max(x), 100)
-ynew = [lagranz(x, y, i) for i in xnew]
-plt.plot(x, y, 'o', xnew, ynew)
-plt.grid(True)
-plt.show()
+
+def interpolation():
+    x, y = getdata()
+    xnew = np.linspace(np.min(x), np.max(x), 100)
+    ynew = [lagranz(x, y, i) for i in xnew]
+    plt.plot(x, y, 'o', xnew, ynew)
+    plt.grid(True)
+    plt.show()
+
+
+
+def approximation():
+    x, y = getdata()
+    n = len(x)  # количество элементов в списках
+    s = sum(y)  # сумма значений y
+    s1 = sum([1 / x[i] for i in range(0, n)])  # сумма 1/x
+    s2 = sum([(1 / x[i]) ** 2 for i in range(0, n)])  # сумма (1/x)**2
+    s3 = sum([y[i] / x[i] for i in range(0, n)])  # сумма y/x
+    a = round((s * s2 - s1 * s3) / (n * s2 - s1 ** 2), 3)  # коэфициент а с тремя дробными цифрами
+    b = round((n * s3 - s1 * s) / (n * s2 - s1 ** 2), 3)  # коэфициент b с тремя дробными цифрами
+    s4 = [a + b / x[i] for i in range(0, n)]  # список значений гиперболической функции
+    so = round(sum([abs(y[i] - s4[i]) for i in range(0, n)]) / (n * sum(y)) * 100, 3)  # средняя ошибка аппроксимации
+    plt.title('Аппроксимация гиперболой Y=' + str(a) + '+' + str(b) + '/x\n Средняя ошибка--' + str(so) + '%', size=14)
+    plt.xlabel('Координата X', size=14)
+    plt.ylabel('Координата Y', size=14)
+    plt.plot(x, y, color='r', linestyle=' ', marker='o', label='Data(x,y)')
+    plt.plot(x, s4, color='g', linewidth=2, label='Data(x,f(x)=a+b/x')
+    plt.legend(loc='best')
+    plt.grid(True)
+    plt.show()
