01b3a7e49d8fa25ba2532bd0b5aca37e13c2dc62
Igor Grebennikov <cidor361@gmail.com>
2020-03-04 17:04:58 +0300

    Made Neurus class

--- Neuron/Neuron.py
+++ Neuron/Neuron.py
@@ -1,37 +1,34 @@
-inputs = [1, 1, 0, 1, 1, 0, 1]
-weights = [0, 0, 0, 0, 0, 0, 0]
-desired_result = 1
-learning_rate = 0.2
-trials = 6
+class Neuron:
+    inputs_list = [1, 1, 0, 1, 1, 0, 1]
+    weight_list = [0, 0, 0, 0, 0, 0, 0]
+    desired_result = 1
+    learning_rate = 0.1
+    trials = 6
+
+    def evaluate_neural_network():
+        result = 0
+        for i in range(len(Neuron.inputs_list)):
+            layer_value = Neuron.inputs_list[i] * Neuron.weight_list[i]
+            result += layer_value
+        print("Оценка нейросети: " + str(result))
+        print("Веса: " + str(Neuron.weight_list))
+        return result
+
+    def evaluate_error():
+        error = Neuron.desired_result - Neuron.actual
+        print("Оценка ошибки: " + str(error))
+        return error
+
+    def learn():
+        print("обучение...")
+        for i in range(len(Neuron.inputs_list)):
+            if Neuron.inputs_list[i] > 0:
+                Neuron.weight_list[i] += Neuron.learning_rate
+
+    def train():
+        for i in range(Neuron.trials):
+            neural_net_result = Neuron.evaluate_neural_network()
+            Neuron.learn()
 
 
-def evaluate_neural_network(input_array, weight_array):
-    result = 0
-    for i in range(len(input_array)):
-        layer_value = input_array[i] * weight_array[i]
-        result += layer_value
-    print("evaluate_neural_network: " + str(result))
-    print("weights: " + str(weights))
-    return result
-
-
-def evaluate_error(desired, actual):
-    error = desired - actual
-    print("evaluate_error: " + str(error))
-    return error
-
-
-def learn(input_array, weight_array):
-    print("learning...")
-    for i in range(len(input_array)):
-        if input_array[i] > 0:
-            weight_array[i] += learning_rate
-
-
-def train(trials):
-    for i in range(trials):
-        neural_net_result = evaluate_neural_network(inputs, weights)
-        learn(inputs, weights)
-
-
-train(trials)
+Neuron.train()
